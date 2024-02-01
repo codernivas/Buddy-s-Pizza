3235e742659d916c7ccfd3f6e4d99d02fc3389aa
nivascoder <nivascoder@gmail.com>
2024-02-01 17:43:16 +0530

    homepage added with header and footer

---
+++ src/components/layout/Hero.js
@@ -0,0 +1,39 @@
+import Image from "next/image"
+import Right from "@/components/icons/Right"
+export default function Hero() {
+  return (
+    <>
+      <section className="grid grid-cols-2 mt-4">
+        <div className="py-12">
+          <h1 className="text-4xl font-semibold">
+            Everything <br />
+            is better <br /> with a&nbsp;{" "}
+            <span className="text-primary"> Pizzaaaa</span>
+          </h1>
+          <p className="my-4 text-gray-500">
+            Taste is the missing piece that makes every day complete, a simple
+            yet delicious joy in life
+          </p>
+          <div className="flex gap-4  text-sm">
+            <button className="flex justify-center bg-primary items-center uppercase flex gap-2 text-white px-4 py-2 rounded-full">
+              Order now
+              <Right />
+            </button>
+            <button className="flex border-0 items-center gap-2 py-2 text-gray-600 font-semibold">
+              Learn more <Right />
+            </button>
+          </div>
+        </div>
+
+        <div className=" relative">
+          <Image
+            src={"/pizza.png"}
+            alt={"pizza"}
+            layout={"fill"}
+            objectFit={"contain"}
+          />
+        </div>
+      </section>
+    </>
+  )
+}
